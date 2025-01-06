4c6ae4f09c56ba4ba485aedb3d516a7c80a5b514
AlexandruChilom <alexchilom30@gmail.com>
2025-01-06 11:44:05 +0100

    Added insfficient money / condition

--- src/Machine.java
+++ src/Machine.java
@@ -15,37 +15,43 @@ public class Machine {
         int valueofpurchasedGoods = sc.nextInt();
         System.out.println("Please insert the money: ");
         int moneyInserted = sc.nextInt();
-        int change = moneyInserted - valueofpurchasedGoods;
 
-        System.out.println("Your change is: " + change);
+        if (moneyInserted < valueofpurchasedGoods) {
+            int change = moneyInserted - valueofpurchasedGoods;
 
-        while (change > 200) {
-            System.out.println("200 lei");
-            change -= 200;
-        }
-        while (change > 100) {
-            System.out.println("100 lei");
-            change -= 100;
-        }
-        while (change > 50) {
-            System.out.println("50 lei");
-            change -= 50;
-        }
-        while (change > 20) {
-            System.out.println("20 lei");
-            change -= 20;
-        }
-        while (change > 10) {
-            System.out.println("10 lei");
-            change -= 10;
-        }
-        while (change > 5) {
-            System.out.println("5 lei");
-            change -= 5;
-        }
-        while (change > 1) {
-            System.out.println("1 leu");
-            change -= 1;
+
+            System.out.println("Your change is: " + change);
+
+            while (change >= 200) {
+                System.out.println("200 lei");
+                change -= 200;
+            }
+            while (change >= 100) {
+                System.out.println("100 lei");
+                change -= 100;
+            }
+            while (change >= 50) {
+                System.out.println("50 lei");
+                change -= 50;
+            }
+            while (change >= 20) {
+                System.out.println("20 lei");
+                change -= 20;
+            }
+            while (change >= 10) {
+                System.out.println("10 lei");
+                change -= 10;
+            }
+            while (change >= 5) {
+                System.out.println("5 lei");
+                change -= 5;
+            }
+            while (change >= 1) {
+                System.out.println("1 leu");
+                change -= 1;
+            }
+        } else {
+            System.out.println("Insufficient funds!");
         }
     }
 }
